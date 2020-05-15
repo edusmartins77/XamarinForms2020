adf1c1af0d65773570d58d0a50c1aa312d23069a
eduardo <edulamy@gmail.com>
2020-05-15 11:26:32 -0300

    APP01 OK

---
+++ Secao05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string nomeEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(nomeEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;                
+        }
+    }
+}
---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Servico.Modelo;
+using App01_ConsultarCEP.Servico;
+
+namespace App01_ConsultarCEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnCEP.Clicked += BuscarCEP;
+        }
+
+        public void BuscarCEP(object sender, EventArgs args)
+        {
+            //TODO - Logica do Programa
+            string cep = txtCEP.Text.Trim().Replace("-", "");
+
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+            //TODO - Validações
+
+            lblResultado.Text = string.Format("Endereco: {0} - {1} - {2} - {3}", end.logradouro+(!string.IsNullOrEmpty(end.complemento) ? " - "+ end.complemento : ""),
+                end.bairro, end.localidade, end.uf);
+        }
+
+    }
+}
